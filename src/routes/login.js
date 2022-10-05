7f16f7545f59141ee259bb98d6942d9afc4f7a12
Marcela <arqmrcl@gmail.com>
2022-10-04 22:39:43 -0300

    04

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,8 +3,27 @@ const randomToken = require('../utils/token');
 
 const app = express.Router();
 
-app.post('/', (_req, res) => {
-    const token = randomToken();
+const INVALID_EMAIL = 'O "email" deve ter o formato "email@email.com"';
+const INVALID_PASSWORD = 'O "password" deve ter pelo menos 6 caracteres';
+const TOKEN = 16;
+
+app.post('/', (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    }
+    if (!password) {
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });    
+    }
+    if (!/[A-z0-9._]+@[a-z]+\.[a-z]{2,3}/.test(email)) {
+        return res.status(400).json({ message: INVALID_EMAIL });
+    }
+    if (password.length < 6) {
+        return res.status(400).json({ message: INVALID_PASSWORD });
+    }
+
+    const token = randomToken(TOKEN);
     return res.status(200).json({ token }); 
 });
 
